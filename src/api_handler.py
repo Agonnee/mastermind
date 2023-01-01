758719d88b7e92545b2656251e44864632a6c0d3
Agonnee <67187469+Agonnee@users.noreply.github.com>
2022-12-31 20:50:29 -0800

    Added error handling and tests for API handler

--- src/api_handler.py
+++ src/api_handler.py
@@ -1,8 +1,25 @@
 import requests
 
-class API_Handler:
 
+class API_Handler:
     def get_code(self) -> str:
-        response = requests.get("https://www.random.org/integers/?num=4&min=0&max=7&col=1&base=10&format=plain&rnd=new")
-        secret_code = response.text.strip().replace("\n","")
-        return secret_code
+
+        try:
+            response = requests.get(
+                "https://www.random.org/integers/?num=4&min=0&max=7&col=1&base=10&format=plain&rnd=new"
+            )
+            response.raise_for_status()
+            secret_code = response.text.strip().replace("\n", "")
+            return secret_code
+        except requests.exceptions.RequestException as err:
+            print("An error has occurred: ", err)
+            raise
+        except requests.exceptions.HTTPError as err_http:
+            print("An HTTP error has occurred: ", err_http)
+            raise
+        except requests.exceptions.ConnectionError as err_con:
+            print("Network connection error: ", err_con)
+            raise
+        except requests.exceptions.Timeout as err_time:
+            print("Timeout Error has occurred: ", err_time)
+            raise
---
+++ tests/test_api_handler.py
@@ -0,0 +1,72 @@
+import pytest
+import requests
+import requests_mock
+from src.api_handler import API_Handler
+
+def test_get_req():
+    """Expect to raise a RequestException"""
+
+    test_handler = API_Handler()
+
+    API_URL = "https://www.random.org/integers/?num=4&min=0&max=7&col=1&base=10&format=plain&rnd=new"
+
+    with requests_mock.Mocker() as mocker:
+        mocker.get(API_URL, exc=requests.exceptions.RequestException)
+
+        with pytest.raises(requests.exceptions.RequestException) as exc:
+            test_handler.get_code()
+
+def test_get_code_http_err():
+    """Expect to raise an HTTPError"""
+
+    test_handler = API_Handler()
+
+    API_URL = "https://www.random.org/integers/?num=4&min=0&max=7&col=1&base=10&format=plain&rnd=new"
+
+    with requests_mock.Mocker() as mocker:
+        mocker.get(API_URL, exc=requests.exceptions.HTTPError)
+        with pytest.raises(requests.exceptions.HTTPError):
+            test_handler.get_code()
+
+def test_get_code_con_err():
+    """Expect to raise a ConnectionError"""
+
+    test_handler = API_Handler()
+
+    API_URL = "https://www.random.org/integers/?num=4&min=0&max=7&col=1&base=10&format=plain&rnd=new"
+
+    with requests_mock.Mocker() as mocker:
+        mocker.get(API_URL, exc=requests.exceptions.ConnectionError)
+
+        with pytest.raises(requests.exceptions.ConnectionError) as exc:
+            test_handler.get_code()
+
+def test_get_code_timeout():
+    """Expect to raise a Timeout"""
+
+    test_handler = API_Handler()
+
+    API_URL = "https://www.random.org/integers/?num=4&min=0&max=7&col=1&base=10&format=plain&rnd=new"
+
+    with requests_mock.Mocker() as mocker:
+        mocker.get(API_URL, exc=requests.exceptions.Timeout)
+
+        with pytest.raises(requests.exceptions.Timeout) as exc:
+            test_handler.get_code()
+
+def test_get_code_correct():
+    """Expect to Return a 4-digit string."""
+
+    test_handler = API_Handler()
+
+    API_URL = "https://www.random.org/integers/?num=4&min=0&max=7&col=1&base=10&format=plain&rnd=new"
+
+    body_text = "0\n0\n0\n1\n\n"
+
+    with requests_mock.Mocker() as mocker:
+        mocker.get(API_URL, status_code=200, text=body_text)
+        response = test_handler.get_code()
+
+    assert response == "0001"
+
+
